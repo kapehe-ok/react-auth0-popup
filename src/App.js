27eac1629e43845230f91b06f54b73dd0cb21922
Luke Oliff <luke@lukeoliff.com>
2018-12-13 22:56:15 +0000

    some suggestions on state management

--- src/App.js
+++ src/App.js
@@ -3,14 +3,45 @@ import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
 import TvShows from "./components/TvShows.js";
 import Movies from "./components/Movies.js";
+import Auth from "./auth/service";
+
+const auth = new Auth();
 
 class App extends Component {
+  loggedIn(state) {
+    // do some bits
+    this.setState({ loggedIn: state.loggedIn });
+  }
+
+  loggedOut(state) {
+    // do some bits
+    this.setState({ loggedIn: state.loggedIn });
+  }
+
+  constructor(props) {
+    super(props);
+
+    auth.loginCallback = this.loggedIn.bind(this);
+    auth.logoutCallback = this.loggedOut.bind(this);
+
+    this.state = { loggedIn: false };
+  }
+
   render() {
     return (
       <BrowserRouter>
         <div>
           <Route component={TvShows} path="/" exact />
           <Route component={Movies} path="/movies" />
+          { this.state.loggedIn ? (
+            <button onClick={() => auth.logout()} className="log-in">
+              Log Out
+            </button>
+          ) : (
+            <button onClick={() => auth.login()} className="log-in">
+              Log In
+            </button>
+          )}
         </div>
       </BrowserRouter>
     );
